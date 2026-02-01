755b7174cf29be4ed728cdde43febde9638f049e
LuckyDadlani <luckydadlani2004@gmail.com>
2026-02-01 19:36:57 +0000

    new changes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,31 @@
+"use strict" // tells compiler to treat all js code as newer version and follow new standards 
+
+// alert( 3+3 ) // we are using node.js not browser 
+
+console.log(3+3)
+console.log("Lucky") // always prefer code readablity 
+
+let name = "lucky"
+let age = 18
+let isLoggedIn = true
+
+/*
+    number => 2^53
+    bigint => bada number
+    string => ""
+    boolean => true/false
+    null => standalone value
+    undefined => abhi value assign nahi hui hai 
+    symbol => used in react generally / unique
+
+
+    object 
+*/
+ 
+console.log(typeof "lucky")
+
+// null ka type is object 
+// undefined ka type is undefined 
+// string ka string bla bla bla 
+
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId  = 144553
+let accountEmail = "lucky@google.com"
+
+// var accountPassword = "12345" : Please never use this it is old and has many problems related to block scope 
+
+accountCity = "Bhopal" // can be done to assign memory to variable but not adviced 
+let accountState // semi colan does not matter (your choice) , if a value is not defined it is takes as undefined
+// accountId = 2 : const keyword cannot be changed
+
+accountEmail = "luckydadlani@gmail.com"
+accountPassword = "1705"
+accountCity = "bengaluru"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
